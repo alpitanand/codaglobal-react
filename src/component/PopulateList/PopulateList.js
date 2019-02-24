bb14946e22acdf99e160885f83e0e618bde28c63
alpitanand <alpitanand20@gmail.com>
2019-02-24 20:00:12 +0530

    Added tooltip

--- src/component/PopulateList/PopulateList.js
+++ src/component/PopulateList/PopulateList.js
@@ -2,23 +2,27 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import classes from './PoulateList.module.css'
+import ReactTooltip from 'react-tooltip'
 
 
 const displayItems = (props)=>{
-
-   
+    
 
     let displayData = props.fetchedData.map((item)=>{
-        return(
-<div className={classes.style}>
-        <ListItem className={classes.list} divider>
-            <ListItemText className={classes.listitem}
-                primary={item.name}
-                secondary={"Song by- "+item.artists}
-            />
         
-        </ListItem>
-        </div>   
+        return(
+            <div className={classes.style} key={item.rank}>
+          <div data-tip={"Rank "+item.rank} > 
+            <ListItem className={classes.list}  divider>
+                <ListItemText className={classes.listitem}
+                    primary={item.name}
+                    secondary={"Song by- "+item.artists}
+                />
+            </ListItem>
+          </div>
+          <ReactTooltip place="bottom" type="dark" effect="solid" >
+          </ReactTooltip>
+            </div>   
         )
     })
     return(
